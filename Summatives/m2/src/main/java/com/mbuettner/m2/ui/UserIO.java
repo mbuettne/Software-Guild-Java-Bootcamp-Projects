063c0314bec78479f8b8fb40145e33f81a8d206b
Matt Buettner <mbuettner92@gmail.com>
2020-02-28 13:42:41 -0600

    commit to change branches

---
+++ Summatives/m2/src/main/java/com/mbuettner/m2/ui/UserIO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mbuettner.m2.ui;
+
+/**
+ *
+ * @author mbuet
+ */
+public interface UserIO {
+    
+}
